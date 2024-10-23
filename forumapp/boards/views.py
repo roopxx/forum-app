e2b52d5a224c9ce2a6cf24f588d316fd07be4e04
roopxx <127219908+roopxx@users.noreply.github.com>
2024-10-23 06:33:31 +0000

    Add models for forum app with django templates

--- forumapp/boards/views.py
+++ forumapp/boards/views.py
@@ -1,4 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Board
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
